bbcdc822400aa7ccc20fbd52854d102a9eedbea5
Brandon Block <brandon.block@pm.me>
2026-04-08 11:07:11 -0500

    take initial arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(ValueEnum, Clone)]
+enum Command {
+    New,
+}
+
+#[derive(Parser)]
+#[command(about = "Search OpenLibrary by title")]
+struct Args {
+    command: Command,
+    // user input title for search
+    title: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{}", &args.title);
 }
