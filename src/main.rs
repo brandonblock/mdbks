f16819ca1fe3faf794d8dc3042fcfee2330b0f02
Brandon Block <brandon.block@pm.me>
2026-04-10 13:52:44 -0500

    move to match arms

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use openlibrary::{SearchResponse, work_fetch};
 enum Command {
     #[default]
     New,
+    Start,
     Finish,
     NotFinish,
     List,
@@ -27,36 +28,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let args = Args::parse();
 
-    let resp: SearchResponse = openlibrary::book_search(&args.title)?;
-    let display_items: Vec<String> = resp
-        .docs
-        .iter()
-        .map(|d| {
-            let author = d
-                .author_name
-                .as_ref()
-                .map(|a| a.join(", "))
-                .unwrap_or_else(|| "Unknown".into());
-            let year = d
-                .first_publish_year
-                .map(|y| y.to_string())
-                .unwrap_or_else(|| "??".into());
-            format!("{} - {} - ({})", d.title, author, year)
-        })
-        .collect();
+    match args.command {
+        Command::New => {
+            let resp: SearchResponse = openlibrary::book_search(&args.title)?;
+            let display_items: Vec<String> = resp
+                .docs
+                .iter()
+                .map(|d| {
+                    let author = d
+                        .author_name
+                        .as_ref()
+                        .map(|a| a.join(", "))
+                        .unwrap_or_else(|| "Unknown".into());
+                    let year = d
+                        .first_publish_year
+                        .map(|y| y.to_string())
+                        .unwrap_or_else(|| "??".into());
+                    format!("{} - {} - ({})", d.title, author, year)
+                })
+                .collect();
 
-    let selection = Select::new()
-        .with_prompt("What do you choose?")
-        .items(&display_items)
-        .interact()
-        .unwrap();
+            let selection = Select::new()
+                .with_prompt("What do you choose?")
+                .items(&display_items)
+                .interact()
+                .unwrap();
 
-    println!("You chose: {}", display_items[selection]);
+            println!("You chose: {}", display_items[selection]);
 
-    let selected = &resp.docs[selection];
-    let mut work_data = work_fetch(&selected.key)?;
-    work_data.authors = selected.author_name.clone();
-    work_data.search_publish_year = selected.first_publish_year;
+            let selected = &resp.docs[selection];
+            let mut work_data = work_fetch(&selected.key)?;
+            work_data.authors = selected.author_name.clone();
+            work_data.search_publish_year = selected.first_publish_year;
 
-    create_new_note(work_data)
+            create_new_note(work_data)
+        }
+        Command::Start => todo!(),
+        Command::Finish => todo!(),
+        Command::NotFinish => todo!(),
+        Command::List => todo!(),
+    }
 }
